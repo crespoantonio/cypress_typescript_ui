2a30e3c0e960c4d6005f7fbe87a7eb05291cb026
Antonio Crespo <lic.crespoantonio@gmail.com>
2024-01-25 23:08:06 -0300

    Add GitHub Action wf

--- cypress/e2e/myAccount.cy.ts
+++ cypress/e2e/myAccount.cy.ts
@@ -2,14 +2,21 @@
 
 import { homePage } from "../pages/home.page";
 import { myAccountPage } from "../pages/myAccount.page";
-
+declare var process: {
+    env:{
+        CYPRESS_USERNAME:string;
+        CYPRESS_PASSWORD:string
+    }
+}
 describe('MY ACCOUNT - LOGIN', function(){
+    const username:string = process.env.CYPRESS_USERNAME || Cypress.env('username');
+    const password:string = process.env.CYPRESS_PASSWORD || Cypress.env('password');
     beforeEach(function(){
         cy.visit('/');
         homePage.clickMyAccountMenu();
     })
     it('Login with valir username and password',function(){
-        myAccountPage.logIn(Cypress.env('username'), Cypress.env('password'));
+        myAccountPage.logIn(username, password);
         myAccountPage.loginMessageWelcome().should('exist').and('include.text', 'tonytest');
     })
     it('Login with incorrect username and incorrect password', function(){
@@ -19,8 +26,8 @@ describe('MY ACCOUNT - LOGIN', function(){
         myAccountPage.loginSection().should('exist').and('be.visible');
     })
     it('Password should be masked', function(){
-        myAccountPage.usernameLabel().type(Cypress.env('username'));
-        myAccountPage.passwordLabel().type(Cypress.env('password'), {log:false});
+        myAccountPage.usernameLabel().type(username);
+        myAccountPage.passwordLabel().type(password, {log:false});
         myAccountPage.passwordLabel().should('have.attr', 'type', 'password');
     })
 })
